358902c52b6895acdabb367c067d63d5329229a6
Prashanth Pai <prashanth.pai@plivo.com>
2019-04-04 12:49:42 +0530

    Fix requeue lua script crash
    Unlike user driven operation such as enqueue, dequeue and finish,
    requeue operation is a server side maintenance operation that
    has to be periodically invoked. The goal of requeue operation is
    to add expired jobs back to the queue if the job hasn't
    exceeded the number of times it can be requeued.
    
    The requeue Lua script fetches multiple keys which may no longer
    exist and hence crashes. This has been fixed at 2 known places
    where exceptions have been observed:
    
    ```
    user_script:45: attempt to perform arithmetic on local 'interval' (a boolean value)
    user_script:21: attempt to compare number with nil
    ```
    
    Signed-off-by: Prashanth Pai <prashanth.pai@plivo.com>

--- sharq/scripts/lua/requeue.lua
+++ sharq/scripts/lua/requeue.lua
@@ -18,7 +18,7 @@ for _, job in pairs(requeue_job_list) do
    local queue_id, job_id = job:match("([^,]+):([^,]+)")
    -- check if the job has any pending requeues.
    local requeues_remaining = redis.call('HGET', KEYS[1] .. ':' .. KEYS[2] .. ':' .. queue_id .. ':requeues_remaining', job_id)
-   if tonumber(requeues_remaining) > -1 then
+   if requeues_remaining and tonumber(requeues_remaining) > -1 then
       -- finite requeues_remaining. decrement by one and check.
       requeues_remaining = requeues_remaining - 1
       -- update the new requeues_remaining value.
@@ -36,16 +36,13 @@ for _, job in pairs(requeue_job_list) do
        redis.call('LPUSH', job_queue_key, job_id)
        -- check if this is the only job in the job queue
        if redis.call('LLEN', job_queue_key) == 1 then
-	  -- check if the time keeper exists
-	  local next_ready_time = 0
-	  if redis.call('EXISTS', KEYS[1] .. ':' .. KEYS[2] .. ':' .. queue_id .. ':time') == 1 then
-	     local last_dequeue_time = redis.call('GET', KEYS[1] .. ':' .. KEYS[2] .. ':' .. queue_id .. ':time')
-	     local interval = redis.call('HGET', KEYS[1] .. ':interval', KEYS[2] .. ':' .. queue_id)
-	     -- compute next ready time
+	  -- time keeper may not exist, so initialize next ready time to now.
+	  local next_ready_time = ARGV[1]
+	  local last_dequeue_time = tonumber(redis.call('GET', KEYS[1] .. ':' .. KEYS[2] .. ':' .. queue_id .. ':time'))
+	  local interval = tonumber(redis.call('HGET', KEYS[1] .. ':interval', KEYS[2] .. ':' .. queue_id))
+	  if last_dequeue_time and interval then
+	     -- time keeper exists, compute next ready time
 	     next_ready_time = last_dequeue_time + interval
-	  else
-	     -- time keeper does not exist. next ready time is now.
-	     next_ready_time = ARGV[1]
 	  end
 	  -- insert this queue into the ready sorted set.
 	  redis.call('ZADD', KEYS[1] .. ':' .. KEYS[2], next_ready_time, queue_id)
