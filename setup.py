4a72c9b4933791500fdd467cca5874cc8f6ad706
varshit97plivo <varshit.battu@plivo.com>
2020-11-11 18:30:30 +0530

    upgrade version from 0.5.3 to 1.0.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='SharQ',
-    version='0.5.3',
+    version='1.0.0',
     url='https://github.com/plivo/sharq',
     author='Plivo Team',
     author_email='hello@plivo.com',
