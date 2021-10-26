6f00bd447b86ed308c4515f2e882bfe6bdb69dcb
Prashanth Pai <prashanth.pai@plivo.com>
2021-10-26 12:35:17 +0530

    Bump up version to 1.2.0
    Signed-off-by: Prashanth Pai <prashanth.pai@plivo.com>

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='SharQ',
-    version='1.1.1',
+    version='1.2.0',
     url='https://github.com/plivo/sharq',
     author='Plivo Team',
     author_email='voice-team@plivo.com',
